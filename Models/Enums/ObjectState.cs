245ef8d408706ba55a8885da80433ab58563d19f
mareklovci <mareklovci@gmail.com>
2022-05-31 11:48:22 +0200

    feat: Create ObjectState enum

---
+++ Models/Enums/ObjectState.cs
@@ -0,0 +1,23 @@
+﻿namespace GrammerIssueTracking.Models.Enums
+{
+    /// <summary>
+    /// Object State Enumeration
+    /// </summary>
+    public enum ObjectState
+    {
+        /// <summary>
+        /// Active
+        /// </summary>  
+        Active = 1,
+
+        /// <summary>
+        /// Logically Deleted
+        /// </summary>
+        Deleted = 2,
+    
+        /// <summary>
+        /// Inactive
+        /// </summary>
+        Inactive = 3
+    }
+}
